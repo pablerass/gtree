8455812cd4abadc3fb89df9f3b4ced18f4c76f7d
Pablo Muñoz <pablerass@gmail.com>
2019-02-26 23:40:45 +0100

    Make it work again and add Search function

--- gtree.go
+++ gtree.go
@@ -5,40 +5,62 @@ import (
 )
 
 type Tree struct {
-    value int
+    key string
+    data string
     left *Tree
     right *Tree
 }
 
-func New(value int) Tree {
-    return Tree{
-        value: value,
+func New(key string, data string) *Tree {
+    return &Tree{
+        key: key,
+        data: data,
+        left: nil,
+        right: nil,
     }
 }
 
-func (t Tree) Insert(value int) {
-    if value < t.value {
+func (t *Tree) Insert(key string, data string) {
+    if key == t.key {
+        t.data = data
+    } else if key < t.key {
         if t.left == nil {
-            newTree := New(value)
-            t.left = &newTree
+            t.left = New(key, data)
         } else {
-            t.left.Insert(value)
+            t.left.Insert(key, data)
         }
     } else {
         if t.right == nil {
-            newTree := New(value)
-            t.right = &newTree
+            t.right = New(key, data)
         } else {
-            t.right.Insert(value)
+            t.right.Insert(key, data)
         }
     }
 }
 
-func (t Tree) Print() {
+func (t *Tree) Search(key string) (string, bool) {
+    if key == t.key {
+        return t.data, true
+    } else if key < t.key {
+        if t.left == nil {
+            return "", false
+        } else {
+            return t.left.Search(key)
+        }
+    } else {
+        if t.right == nil {
+            return "", false
+        } else {
+            return t.right.Search(key)
+        }
+    }
+}
+
+func (t *Tree) Print() {
     if t.left != nil {
         t.left.Print()
     }
-    fmt.Println(t.value)
+    fmt.Println(t.key)
     if t.right != nil {
         t.right.Print()
     }
