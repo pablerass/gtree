0f303fc790c8165efbb349c1dcaf9bcdf839c0a4
Pablo Muñoz <pablerass@gmail.com>
2019-03-30 07:53:39 +0100

    Move search method to bst only

--- bst.go
+++ bst.go
@@ -7,3 +7,29 @@ type BinarySearchTree struct {
 type binarySearchNode struct {
     binaryNode
 }
+
+func (t BinarySearchTree) Search(key string) (string, bool) {
+    if t.root == nil {
+        return "", false
+    } else {
+        return t.root.search(key)
+    }
+}
+
+func (n binarySearchNode) search(key string) (string, bool) {
+    if key == n.key {
+        return n.data, true
+    } else if key < n.key {
+        if n.left == nil {
+            return "", false
+        } else {
+            return n.left.search(key)
+        }
+    } else {
+        if n.right == nil {
+            return "", false
+        } else {
+            return n.right.search(key)
+        }
+    }
+}
